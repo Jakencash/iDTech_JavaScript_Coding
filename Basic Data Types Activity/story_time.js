de250dda4aa0f8debe73ddc91d281196e8218760
Jake Roth <jakencash@gmail.com>
2022-06-22 16:12:52 -0700

    new file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,32 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Greetings traveler"
+let name = "Morshu"
+let title = "Shopkeeper"
+let worldName = "Kingdom of Hyrule"
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 37
+let year = 688
+let century = 7
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let ItemShop = ["Lamp Oil","Ropes","Bombs"]
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasFavItem = true
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings +" My name is " + name + "." )
+console.log(" I am" + " The " + title)
+console.log(" and" +" Its The " + century + "th century .")
+console.log(" I just turned " + age + " so it would be nice if you spend so more extra gems." + "\n")
+console.log("Traveler: I have a bit of a budgit so i cant spend much")
+console.log(" Morshu: Well whats your favorite item ?")
+console.log(" Traveler:" + hasFavItem + ", my favorite item is " + ItemShop[3] + 
+"." + "\n")
+console.log("Feel free to explore all are avalible stock")
